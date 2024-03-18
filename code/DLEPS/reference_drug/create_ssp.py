3362fe2532dcf96b64e5a0a15b5f6e57b18804b7
Kusal Debnath <debnathk1997@gmail.com>
2024-03-18 16:24:54 -0400

    Code added

--- code/DLEPS/reference_drug/create_ssp.py
+++ code/DLEPS/reference_drug/create_ssp.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-root = '/lustre/home/debnathk/dleps/code/DLEPS/reference_drug/'
+root = 'code/DLEPS/reference_drug/'
 
 # Read fingerprints
 df_fgrps_train = pd.read_csv(root + 'fgrps_train.csv')
@@ -17,29 +17,36 @@ for i in range(df_fgrps_train.shape[0]):
     ssp_list.append(ssp)
 
 ssp_stack = np.stack(ssp_list).astype(int)
-ssp_stack.shape
+print(ssp_stack.shape)
 
 # Split train, test
-TEST_SIZE = 75
-ssp_train = ssp_stack[TEST_SIZE:]
-ssp_test = ssp_stack[:TEST_SIZE]
+# TEST_SIZE = 75
+# ssp_train = ssp_stack[TEST_SIZE:]
+# ssp_test = ssp_stack[:TEST_SIZE]
 
 # Save dataset
 import h5py
 
-h5f = h5py.File(root + 'ssp_data_train.h5', 'w')
-h5f.create_dataset('data', data=ssp_train)
+h5f = h5py.File(root + 'ssp_data.h5', 'w')
+h5f.create_dataset('data', data=ssp_stack)
 h5f.close()
 
-h5f = h5py.File(root + 'ssp_data_test.h5', 'w')
-h5f.create_dataset('data', data=ssp_test)
-h5f.close()
+# h5f = h5py.File(root + 'ssp_data_train.h5', 'w')
+# h5f.create_dataset('data', data=ssp_train)
+# h5f.close()
+
+# h5f = h5py.File(root + 'ssp_data_test.h5', 'w')
+# h5f.create_dataset('data', data=ssp_test)
+# h5f.close()
 
 # Read datset
-h5f = h5py.File(root + 'ssp_data_train.h5', 'r')
-ssp_train = h5f['data'][:]
-h5f = h5py.File(root + 'ssp_data_test.h5', 'r')
-ssp_test = h5f['data'][:]
+h5f = h5py.File(root + 'ssp_data.h5', 'r')
+ssp_data = h5f['data'][:]
+# h5f = h5py.File(root + 'ssp_data_train.h5', 'r')
+# ssp_train = h5f['data'][:]
+# h5f = h5py.File(root + 'ssp_data_test.h5', 'r')
+# ssp_test = h5f['data'][:]
 
-print(ssp_train.shape)
-print(ssp_test.shape)
+print(ssp_data.shape)
+# print(ssp_train.shape)
+# print(ssp_test.shape)
